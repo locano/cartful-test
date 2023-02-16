901023a24480f6a9cd60982e68dbd33234adc148
ludwing.cano <ludwing238@gmail.com>
2023-02-15 19:10:17 -0600

    Finish Text problem 2

--- problemas/cartful-problem2/problem2.js
+++ problemas/cartful-problem2/problem2.js
@@ -33,11 +33,31 @@ function createMessate() {
 
   let message = document.createElement('p');
   message.id = 'result-message';
-  message.innerHTML = 'Add 2 of this product and the third one is free.';
   message.style.backgroundColor = '#D0EBE7';
   message.style.color = 'black';
   let parent = document.getElementById('afterpay-messaging-widget');
   if (parent) {
+    let f1 = document.getElementById('product_form_6553738543222');
+    let f1_childen = f1.lastChild.children[1];
+    let quantity = f1_childen.children['quantity'].value;
+    switch (Number(quantity)) {
+      case 1:
+        message.innerHTML = 'Add 2 of this product and the third one is free.';
+        break;
+      case 2:
+        message.innerHTML = 'Add another one free to your cart';
+        break;
+      case 3:
+        message.innerHTML = 'Congrats! add to cart now!';
+        break;
+      default:
+        if(quantity > 3){
+        message.innerHTML = window.prompt("Enter your message");;
+        }
+        break;
+    }
+
+
     parent.append(message);
   }
 }
