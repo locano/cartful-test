2aa5d022e0dd9417d4cbb3d9d83c02312fc78578
ludwing.cano <ludwing238@gmail.com>
2023-02-15 18:57:31 -0600

    Finish problem 2

--- problemas/cartful-problem2/problem2.js
+++ problemas/cartful-problem2/problem2.js
@@ -1,6 +1,50 @@
-const form = document.querySelector('#product_form_6553738543222');
+let form = document.querySelector('#product_form_6553738543222');
 
-  form.addEventListener('click', (event) => {
-    event.preventDefault();
-    // Adding Script logic
-  });
+form.addEventListener('click', (event) => {
+  event.preventDefault();
+  // Adding Script logic
+  let product = event.srcElement.firstChild.data;
+  let cleanProduct = product.replace(/(\r\n|\n|\r| )/gm, "");
+
+
+  switch (cleanProduct) {
+    case '18-24months':
+      createMessate();
+      break;
+    case '2T':
+      createMessate();
+      break;
+    case '3T':
+      createMessate();
+      break;
+    default:
+      removeMessage();
+      break;
+  }
+});
+
+function createMessate() {
+
+  let find = document.getElementById('result-message');
+
+  if (find) {
+    find.remove();
+  }
+
+  let message = document.createElement('p');
+  message.id = 'result-message';
+  message.innerHTML = 'Add 2 of this product and the third one is free.';
+  message.style.backgroundColor = '#D0EBE7';
+  message.style.color = 'black';
+  let parent = document.getElementById('afterpay-messaging-widget');
+  if (parent) {
+    parent.append(message);
+  }
+}
+
+function removeMessage() {
+  let find = document.getElementById('result-message');
+  if (find) {
+    find.remove();
+  }
+}
